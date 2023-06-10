e8f8a0f0a54151eb5d5aef4b0b9f8f2e69508d14
Parisa Sadeghi <pariisadeqii@gmail.com>
2023-06-10 14:29:48 +0200

    Adding chat gpt apies

---
+++ chat-gpt/gpt.js
@@ -0,0 +1,45 @@
+const form = document.getElementById('chat-form');
+const mytextInput = document.getElementById('mytext');
+const jobdescriptionInput = document.getElementById('jobdescription');
+const responseTextarea = document.getElementById('response');
+
+const API_KEY = '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';
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const mytext = mytextInput.value.trim();
+	const jobdescription = jobdescriptionInput.value.trim();
+	const blah= `Use this resume"${mytext}" and write a cover letter for this job "${jobdescription}"`
+	console.log(blah)
+    if (mytext) {
+        try {
+            const response = await fetch('https://api.openai.com/v1/chat/completions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${API_KEY}`,
+                },
+                body: JSON.stringify({
+                    model: 'gpt-3.5-turbo',
+                    messages: [{ role: 'user', content: blah}],
+                    temperature: 1.0,
+                    top_p: 0.7,
+                    n: 1,
+                    stream: false,
+                    presence_penalty: 0,
+                    frequency_penalty: 0,
+                }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                responseTextarea.value = data.choices[0].message.content;
+            } else {
+                responseTextarea.value = 'Error: Unable to process your request.';
+            }
+        } catch (error) {
+            console.error(error);
+            responseTextarea.value = 'Error: Unable to process your request.';
+        }
+    }
+});
